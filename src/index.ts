e3b41c0d7638cc1872b85243f9581b3d92494bab
Ron Borysovski <roni537@gmail.com>
2019-03-17 17:44:26 +0200

    refactor: use new logger

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,20 @@ import * as mongoose from 'mongoose';
 import { config } from './config';
 import { Classification } from './classifications/classification';
 import { Pp } from './pps/pp';
+import { log } from './utils/logger';
 
 (async () => {
     await mongoose.connect(
         config.db.connectionString,
         { useNewUrlParser: true },
     );
+    log('verbose', 'Server', 'Server started');
 
     const updaterJob = new CronJob(config.cron.taskTimer, async function () {
-        console.log(`Classifications update @ ${new Date()}`);
+        log('info', 'Cron', 'Classifications update started');
         await Classification.updateSources();
         await Pp.updatePps();
+        log('info', 'Cron', 'Classifications updated successfully');
     });
 
     updaterJob.start();
